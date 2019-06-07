b8bd2c9ec2d94822d3f5f901a0108312105a99ec
JAVA PRACTICE <amansinghal.it1@gmail.com>
2019-06-07 12:05:38 +0530

    Changes during practice

--- app/src/main/java/com/appsquadz/myfirst/SplashActivity.java
+++ app/src/main/java/com/appsquadz/myfirst/SplashActivity.java
@@ -2,6 +2,8 @@ package com.appsquadz.myfirst;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,6 +11,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Button button=findViewById(R.id.btn_next);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+
+            }
+        });
 
     }
 }
